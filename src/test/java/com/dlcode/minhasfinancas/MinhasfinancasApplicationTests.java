89a933a9fbe17294b2d5d5b36e3df6e0ae7c49d3
Paloma-Regis-Ferreira <carlos_daniel_921@outlook.com>
2022-01-27 19:00:05 -0300

    Inicio do projeto

---
+++ src/test/java/com/dlcode/minhasfinancas/MinhasfinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlcode.minhasfinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasfinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
